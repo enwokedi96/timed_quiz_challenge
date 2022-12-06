df6713f81ebd5b76072136ec4223db1180b24bb5
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-06 07:07:43 +0000

    get relevant selectors from index.html

---
+++ assets/js/logic.js
@@ -0,0 +1,6 @@
+var startQuizButton = document.getElementById("start");
+var startPage = document.querySelector(".start");
+var currentQuestion = document.getElementById("questions");
+var solutionPage = document.querySelector("#end-screen");
+var initials = document.querySelector("#initials");
+var submitInitials = document.querySelector("#submit");
