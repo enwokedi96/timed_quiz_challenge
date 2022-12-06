cd9c83d828b4f671607d4bc947b5fb2f8fa257f1
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-06 07:15:38 +0000

    initializing function for loading questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,25 @@
+
+var questionCount =  0;
+
 var startQuizButton = document.getElementById("start");
 var startPage = document.querySelector(".start");
 var currentQuestion = document.getElementById("questions");
 var solutionPage = document.querySelector("#end-screen");
 var initials = document.querySelector("#initials");
 var submitInitials = document.querySelector("#submit");
+
+
+
+function revealQuestions(event) {
+    var i = questionCount;
+
+    currentQuestion.setAttribute('class','hide');
+    console.log('Finished!')
+    solutionPage.removeAttribute('class','hide');
+    
+ 
+    startPage.setAttribute('class','fadeOut')
+    startPage.setAttribute('class','hide');
+        
+}
+
