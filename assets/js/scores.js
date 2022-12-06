3e42cf91ac1a27532e2b3c01bdec6ab4ff97dbe9
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-06 16:18:19 +0000

    highscore page now displays all logged results

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -3,12 +3,15 @@ var displayScore = document.querySelector("#final-score");
 var display = document.querySelector("#highscores");
 //var userScore = localStorage.getItem("currentScore");
 
-var lastUser = JSON.parse(localStorage.getItem("playerHistory_1"));
-console.log(lastUser);
-var displayItem = document.createElement("li");
-displayItem.textContent = lastUser.user + ' ----------------------------------------- ' + lastUser.score;
-console.log(displayItem);
-display.appendChild(displayItem);
+var lastUsers = JSON.parse(localStorage.getItem("playerHistory_1"));
+//console.log(lastUsers);
+display.appendChild(document.createTextNode('(current scores at last rung!)'));
+for (let i=0; i<lastUsers.length; i++){
+    console.log(lastUsers[i])
+    var displayItem = document.createElement("li");
+    displayItem.textContent = lastUsers[i][0] + ' ----------------------------------------- ' + lastUsers[i]["1"];
+    console.log(displayItem);
+    display.appendChild(displayItem);}
 
 
 
