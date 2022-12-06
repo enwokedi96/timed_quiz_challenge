7ac814593ffadc79a99682aacfd68993ba626185
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-06 06:58:49 +0000

    some questions added, structure is nested objects

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var allQuestions = {
+    1:{
+        question: 'What skill does a front-end developer not need?',
+        options:['React', 'MATLAB Programming', 'DOM Manipulation', 'Javascript'],
+        answer: 1},
+    2:{
+        question: 'Which of these describes HTML meta tags',
+        options:['Meta tags can include data about encoding, document title, character description, etc,',
+         'Meta tags are not passed as pairs', 
+         'Meta tags are displayed on the page', 
+         'Meta tags fit inside the HTML body tag.'],
+        answer: 0},
+    3:{
+        question: 'Differentiate between div and span?',
+        options:['Div is for in-line elements and span for blocks', 
+        'Span tag cannot include global and attributes',
+        'Div can contain <p> and span does not'],
+        answer: 2},
+    4:{
+        question: 'What is a Grid system in CSS?',
+        options:['Writing neat, legible text unto HTML body', 
+            'Liasing with JavaScripts to implement user-informed styles',
+            'Partitioning of spatial dimensions of page into relevant html contents', 
+            'Porting styles from external sources to local CSS'],
+        answer: 2 
+    }
+}
