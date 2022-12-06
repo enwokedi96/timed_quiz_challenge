1eb56fcb6dcca1a8d3c72c7f867ebd3fca053f2b
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-06 13:04:41 +0000

    removed redundant bits

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,10 +1,7 @@
-//import  * as logic from './logic.js'
 
 var displayScore = document.querySelector("#final-score"); 
 var display = document.querySelector("#highscores");
 var userScore = localStorage.getItem("currentScore");
-
-//console.log(localStorage.getItem(""));
 
 if(displayScore){
     displayScore.textContent = userScore;
@@ -19,3 +16,4 @@ display.appendChild(displayItem);
 
 
 
+                                                                                             
