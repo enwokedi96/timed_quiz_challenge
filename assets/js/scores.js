ca5575d74042e08397e60529be008a2b944818dd
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-06 16:37:32 +0000

    added event listener/func to clear screen and log

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,17 +1,28 @@
 
 var displayScore = document.querySelector("#final-score"); 
 var display = document.querySelector("#highscores");
-//var userScore = localStorage.getItem("currentScore");
+var clearLeaderboard = document.querySelector("#clear")
 
 var lastUsers = JSON.parse(localStorage.getItem("playerHistory_1"));
-//console.log(lastUsers);
+clearLeaderboard.addEventListener('click', clearAllLogs);
+
 display.appendChild(document.createTextNode('(current scores at last rung!)'));
+
 for (let i=0; i<lastUsers.length; i++){
     console.log(lastUsers[i])
     var displayItem = document.createElement("li");
     displayItem.textContent = lastUsers[i][0] + ' ----------------------------------------- ' + lastUsers[i]["1"];
     console.log(displayItem);
     display.appendChild(displayItem);}
+
+function clearAllLogs() {
+        let node = document.getElementById("highscores")
+        while(node.hasChildNodes()) {
+          node.removeChild(node.lastChild);
+        }
+        localStorage.clear();
+        sessionStorage.clear();
+     }
 
 
 
