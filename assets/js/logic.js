e3bef8e0f121aa9000bd5640d76af86440bc81b4
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-06 07:30:45 +0000

    refined question loader, stops if queestion finish

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -13,41 +13,47 @@ startQuizButton.addEventListener('click',revealQuestions);
 
 async function revealQuestions(event) {
     var i = questionCount;
-
     clearCurrentQuestions(); 
 
     areThereStillQuestions = (allQuestions[Object.keys(allQuestions)[questionCount]]!==undefined)
     console.log(areThereStillQuestions);
+    if (areThereStillQuestions===false) {
+        currentQuestion.setAttribute('class','hide');
+        console.log('Finished!')
+        solutionPage.removeAttribute('class','hide');
+        //startQuiz.removeEventListener('click',revealQuestions);
+        //currentQuestion.removeEventListener('click', checkSolution);
+    }
 
-    currentQuestion.setAttribute('class','hide');
-    console.log('Finished!')
-    solutionPage.removeAttribute('class','hide');
-    
-    startPage.setAttribute('class','fadeOut')
-    let d = await delay(1000);
-    startPage.setAttribute('class','hide');
+    else{
+        if (i==0){
+            startPage.setAttribute('class','fadeOut')
+            let d = await delay(1000);
+            startPage.setAttribute('class','hide');
+        }
 
-    var askQuestion = document.createElement("h2");
-    askQuestion.textContent = allQuestions[Object.keys(allQuestions)[i]]["question"];
-    currentQuestion.appendChild(askQuestion);
-    for (let j=0; j<allQuestions[Object.keys(allQuestions)[i]]["options"].length; j++){
-        var opt = allQuestions[Object.keys(allQuestions)[i]]["options"][j];
-        var selectOpt = document.createElement("button");
-        var selectOptLabel = opt; //console.log(selectOptLabel)
-        setSelectAttributes(selectOpt, {"id":`${j}`,
-                                        "style": `text-align: left;`})
-        selectOpt.onclick = function(e) {
-            console.log(this.id);
-            userAnswer = this.id;};
-        var optText = document.createTextNode(`option ${j+1} :${opt}`); //document.createElement("label");       
-        selectOpt.appendChild(optText);
-        currentQuestion.appendChild(selectOpt); 
-        currentQuestion.appendChild(document.createElement("br"));
-        let d = await delay(200);
-        
+        var askQuestion = document.createElement("h2");
+        askQuestion.textContent = allQuestions[Object.keys(allQuestions)[i]]["question"];
+        currentQuestion.appendChild(askQuestion);
+        for (let j=0; j<allQuestions[Object.keys(allQuestions)[i]]["options"].length; j++){
+            var opt = allQuestions[Object.keys(allQuestions)[i]]["options"][j];
+            var selectOpt = document.createElement("button");
+            var selectOptLabel = opt; //console.log(selectOptLabel)
+            setSelectAttributes(selectOpt, {"id":`${j}`,
+                                            "style": `text-align: left;`})
+            selectOpt.onclick = function(e) {
+                console.log(this.id);
+                userAnswer = this.id;};
+            var optText = document.createTextNode(`option ${j+1} :${opt}`); //document.createElement("label");       
+            selectOpt.appendChild(optText);
+            currentQuestion.appendChild(selectOpt); 
+            currentQuestion.appendChild(document.createElement("br"));
+            let d = await delay(200);
+        }
+        currentAnswer = allQuestions[Object.keys(allQuestions)[questionCount]]["answer"]
+    }
+
 }
-}
-
 // -------------------------- ALL HELPER  FUNCTIONS ---------------------------------//
 
 const delay = (delayInms) => {
