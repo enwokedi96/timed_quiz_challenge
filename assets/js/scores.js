59f80edbf0adf686896f7b09d9d568d6f5659359
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-06 07:53:17 +0000

    append user and score items to ordered list

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -9,3 +9,13 @@ var userScore = localStorage.getItem("currentScore");
 if(displayScore){
     displayScore.textContent = userScore;
 }
+
+var lastUser = JSON.parse(localStorage.getItem("playerHistory"));
+console.log(lastUser);
+var displayItem = document.createElement("li");
+displayItem.textContent = lastUser.user + ' ----------------------------------------- ' + lastUser.score;
+console.log(displayItem);
+display.appendChild(displayItem);
+
+
+
